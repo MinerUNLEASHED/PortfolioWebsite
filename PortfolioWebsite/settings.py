3fa96a663228d11996a2e42a7f9f4f3f84d49ddf
MinerUNLEASHED <2020tray@gmail.com>
2024-11-29 10:22:34 -0600

    working on static files

--- PortfolioWebsite/settings.py
+++ PortfolioWebsite/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static")
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 # URL prefix for static files
 # STATIC_URL = '/static/'
