99b8128cc5a98f8237e4ede33b7aedbb4fde849a
MinerUNLEASHED <2020tray@gmail.com>
2024-11-29 10:46:07 -0600

    working on static files

--- PortfolioWebsite/settings.py
+++ PortfolioWebsite/settings.py
@@ -119,11 +119,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static")
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_URL = '/static/'
+# # STATICFILES_DIRS = [
+# #     os.path.join(BASE_DIR, "static")
+# # ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
 
 # URL prefix for static files
 # STATIC_URL = '/static/'
